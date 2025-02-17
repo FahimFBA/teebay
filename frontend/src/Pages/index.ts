0be679580a783f45e50acbb7cbc99cc2bdc1bac8
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2025-02-18 00:29:25 +0600

    routes and pages added

---
+++ frontend/src/Pages/index.ts
@@ -0,0 +1,7 @@
+export * from "./Home";
+export * from "./Settings";
+export * from "./Products";
+export * from "./ProductDetails";
+export * from "./ProfileDetails";
+export * from "./Login";
+export * from "./Register";
