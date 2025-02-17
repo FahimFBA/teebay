1a1dd803faf61d9b5b3e88eb2fc96706cb68111d
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2025-02-18 00:18:49 +0600

    add: shadcn UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Button } from "./components/ui/button";
 
 const App = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <Button>Hello!</Button>
+    </h1>
+  );
 };
 
 export default App;
